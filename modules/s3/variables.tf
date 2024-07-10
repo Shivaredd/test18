0617027b8b266aa149bd1976669e4733c1c6add6
shivaredd <buggaraghu@gmail.com>
2024-07-10 17:41:44 +0530

    s3

---
+++ modules/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "bucket_tags" {
+  description = "A map of tags to assign to the bucket"
+  type        = map(string)
+  default     = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
