1bdb2dfe0c9fc1669dacc5c1ef37902a196113fb
Shivaredd <35751968+Shivaredd@users.noreply.github.com>
2024-07-15 15:46:26 +0530

    Update variables.tf

--- modules/sg/variables.tf
+++ modules/sg/variables.tf
@@ -1,15 +1,27 @@
 variable "vpc_id" {
-  description = "The ID of the VPC where the security group will be created."
+  description = "The ID of the VPC where the security group will be created"
   type        = string
 }
 
+variable "http_cidr_blocks" {
+  description = "List of CIDR blocks allowed to access HTTP"
+  type        = list(string)
+}
+
+variable "https_cidr_blocks" {
+  description = "List of CIDR blocks allowed to access HTTPS"
+  type        = list(string)
+}
+
 variable "vpc_cidr_block" {
-  description = "The CIDR block of the VPC."
+  description = "The CIDR block for the VPC"
   type        = string
 }
 
 variable "vpc_ipv6_cidr_block" {
-  description = "The IPv6 CIDR block of the VPC."
+  description = "The IPv6 CIDR block for the VPC"
   type        = string
 }
+
+
 
