b3acc441bb39aa89eb86f6e26c795c5a0dbc1780
Shivaredd <35751968+Shivaredd@users.noreply.github.com>
2024-07-15 17:31:45 +0530

    Update main.tf

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,20 +1,31 @@
-resource "aws_security_group" "my_security_group" {
-  vpc_id = var.vpc_id
+  resource "aws_security_group" "this" {
+  name        = var.name
+  description = var.description
+  vpc_id      = var.vpc_id
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.http_cidr_blocks
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+      description = ingress.value.description
+    }
   }
 
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = var.https_cidr_blocks
+  dynamic "egress" {
+    for_each = var.egress_rules
+
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+      description = egress.value.description
+    }
   }
 
-  
+  tags = var.tags
+}
