758d20648d56b0fe41298a1bc6e2a5d7b85853d0
Shivaredd <35751968+Shivaredd@users.noreply.github.com>
2024-07-15 17:32:24 +0530

    Update variables.tf

--- modules/sg/variables.tf
+++ modules/sg/variables.tf
@@ -1,16 +1,46 @@
-variable "vpc_id" {
-  description = "The ID of the VPC where the security group will be created"
+variable "name" {
   type        = string
+  description = "The name of the security group"
 }
 
-variable "http_cidr_blocks" {
-  description = "List of CIDR blocks allowed to access HTTP"
-  type        = list(string)
+variable "description" {
+  type        = string
+  description = "The description of the security group"
 }
 
-variable "https_cidr_blocks" {
-  description = "List of CIDR blocks allowed to access HTTPS"
-  type        = list(string)
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID where the security group will be created"
+}
+
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+  description = "List of ingress rules"
+  default     = []
+}
+
+variable "egress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+    description = string
+  }))
+  description = "List of egress rules"
+  default     = []
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of tags to add to all resources"
+  default     = {}
 }
 
 
